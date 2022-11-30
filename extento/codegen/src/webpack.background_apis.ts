eb4ac998eab2914ff8e20b5230f9f2b5ebf6ddf0
Colin Campbell <colinlaptop@Colins-MacBook-Pro.local>
2022-11-30 11:57:35 -0500

    auto-commit

---
+++ extento/codegen/src/webpack.background_apis.ts
@@ -0,0 +1,5 @@
+import * as __template__ from '@workspaces/__template__/background_api';
+
+export default {
+    __template__,
+};
---
+++ extento/codegen/src/webpack.content_script_processes.ts
@@ -0,0 +1,5 @@
+import * as __template__ from '@workspaces/__template__/content_script_process';
+
+export default {
+    __template__,
+};
