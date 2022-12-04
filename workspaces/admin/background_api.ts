638eb73193a9cf4956ed1eff758d371cc6a16b61
Colin Campbell <colinlaptop@Colins-MacBook-Pro.local>
2022-12-04 14:30:24 -0500

    last

--- workspaces/admin/background_api.ts
+++ workspaces/admin/background_api.ts
@@ -1,7 +1,5 @@
 import config_cacher from '@extento/shared/config_cacher';
 
-const somefunc = () => null;
-
 export const example = async () => {
     const config = await config_cacher.get();
     console.info('ran example function with config', config);
