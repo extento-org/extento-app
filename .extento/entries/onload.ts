00186e1bcb916cffe864acec555136be41e5ab94
Colin Campbell <colinlaptop@Colins-MacBook-Pro.local>
2022-12-06 17:10:16 -0500

    auto-commit

--- .extento/entries/onload.ts
+++ .extento/entries/onload.ts
@@ -1,5 +1,3 @@
 import define_onloads from '@extento.api/window.define_onloads';
 
-import './polyfill.js';
-
 define_onloads();
--- .extento/entries/ui.tsx
+++ .extento/entries/ui.tsx
@@ -1,5 +1,3 @@
 import create_react_root from '@extento.api/content.create_react_root';
 
-import './polyfill.js';
-
 create_react_root();
