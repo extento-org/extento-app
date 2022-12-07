628e6cd83064517a3c22dab855faf090ac69ab42
Colin Campbell <colinlaptop@Colins-MBP.attlocal.net>
2022-12-06 23:21:15 -0500

    auto-commit

---
+++ .extento/codegen/webpack.manifests.ts
@@ -0,0 +1,17 @@
+// @ts-nocheck
+
+import { manifest } from '@extento.api/utils.strip_build';
+
+import * as admin from '@_workspace/admin/manifest.json';
+import * as candidate from '@_workspace/candidate/manifest.json';
+import * as devops from '@_workspace/devops/manifest.json';
+import * as manager from '@_workspace/manager/manifest.json';
+import * as tester from '@_workspace/tester/manifest.json';
+
+export default {
+    admin: manifest<typeof admin>('admin', admin),
+    candidate: manifest<typeof candidate>('candidate', candidate),
+    devops: manifest<typeof devops>('devops', devops),
+    manager: manifest<typeof manager>('manager', manager),
+    tester: manifest<typeof tester>('tester', tester),
+};
---
+++ .extento/codegen/webpack.onloads.ts
@@ -0,0 +1,17 @@
+// @ts-nocheck
+
+import { onload } from '@extento.api/utils.strip_build';
+
+import * as admin from '@_workspace/admin/onload';
+import * as candidate from '@_workspace/candidate/onload';
+import * as devops from '@_workspace/devops/onload';
+import * as manager from '@_workspace/manager/onload';
+import * as tester from '@_workspace/tester/onload';
+
+export default {
+    admin: onload<typeof admin>('admin', admin),
+    candidate: onload<typeof candidate>('candidate', candidate),
+    devops: onload<typeof devops>('devops', devops),
+    manager: onload<typeof manager>('manager', manager),
+    tester: onload<typeof tester>('tester', tester),
+};
